1e7c683fca0435ede084578f76ce2fddb26df351
ubinix-warun <warun@ubinix.com>
2021-01-13 23:15:21 +0700

    ADD: 3.-querying-the-secret-blockchain

---
+++ 3.-querying-the-secret-blockchain/query.js
@@ -0,0 +1,31 @@
+const {
+    CosmWasmClient
+  } = require("secretjs");
+  
+  require('dotenv').config();
+  
+  const main = async () => {
+    // Create connection to DataHub Secret Network node
+    const client = new CosmWasmClient(process.env.SECRET_REST_URL)
+  
+    // 1. Query node info
+    const nodeInfo = await client.restClient.nodeInfo();
+    console.log('nodeInfo: ', nodeInfo);
+
+    // 2. Query latest blocks
+    const blocksLatest = await client.restClient.blocksLatest();
+    console.log('blocksLatest: ', blocksLatest);
+    
+    // await client.restClient.blocks(992156);
+
+    // 3. Query account
+    const account = await client.getAccount(process.env.ADDRESS)
+    console.log('Account: ', account);
+
+  }
+  
+  main().then(resp => {
+    console.log(resp);
+  }).catch(err => {
+    console.log(err);
+  })
